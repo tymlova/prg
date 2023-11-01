9a17df4d4eb4e6527c3db9c97bd73868fc4bd9d4
Tymlova Alzbeta <tymlova@gymvod.local>
2023-11-01 13:17:20 +0100

    hmw02

---
+++ hmw02/hmw02/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hmw02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("zadejte čísla a & b (a je počet řádků, b je počet sloupců)");
+          /*  int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());*/
+            string strA = Console.ReadLine();
+            string strB = Console.ReadLine();
+            
+            Console.WriteLine(" ");
+            Console.WriteLine("toto je vaše pole:");
+            int[,] myArray = new int[a,b];
+            Random rnd = new Random();
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    myArray[i, j] = rnd.Next(1, a * b);
+                    Console.Write(myArray[i, j]+" ");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine(" ");
+            //textwall
+            Console.WriteLine("Zadejte číslo dle operace, kterou chcete udělat");
+            Console.WriteLine("1 - prohazování prvků");
+            Console.WriteLine("2 - prohazování řádků");
+            Console.WriteLine("3 - prohazování sloupců");
+            int operation = Convert.ToInt32(Console.ReadLine());
+
+            //switch 
+            switch (operation)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                
+            }
+
+            Console.ReadKey();
+        }
+        
+        public static bool TryParse(string strA, out int[,] result)
+        {
+
+        }
+    }
+}
