c0dae93ce7748dc6a749f93d44d601773aa32300
Tymlova Alzbeta <tymlova@gymvod.local>
2023-12-12 11:23:46 +0100

    rectangle

---
+++ ClassPlayground/ClassPlayground/Rectangle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPlayground
+{
+    public class Rectangle
+    {
+        public int width ;
+        public int height;
+        public int CalculateArea(int width, int height)
+        {
+            return width * height;
+        }
+
+        public void CalculateAspectRatio(int width, int height)
+        {
+
+           
+           if (width < height)
+            {
+                for (int i = 2; i <= width; i++)
+                {
+                    if (width % i == 0 && height%i==0)
+                    {
+                        width = width / i;
+                        height = height / i;
+                    }
+                    
+                    
+                }
+                Console.WriteLine("poměr stran je " + width + " : " + height + ", obdélník je vysoký");
+            }
+           else if (height < width)
+            {
+                for (int i = 2; i <= height; i++)
+                {
+                    if (width % i == 0 && height % i == 0)
+                    {
+                        width = width / i;
+                        height = height / i;
+                    }
+                    
+                   
+                    
+                    
+                }
+                Console.WriteLine("poměr stran je " + width + " : " + height + ", obdélník je široký");
+            }
+           else 
+            {
+                Console.WriteLine("obdélník je čtverec, proto je v poměru 1 : 1");
+            }
+        }
+    }
+}
---
+++ classes/classes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    internal class Program
+    {
+        class Animal
+        {
+            public string name;
+            public int avgAge;
+            public void AnimalNoise()
+            {
+                Console.WriteLine("*happy noise*");
+            }
+        }
+
+        class Dog : Animal 
+        {
+            public string race;
+        }  
+
+        class Cat : Dog
+        { 
+        
+        }
+
+        static void Main(string[] args)
+        {
+            Dog newDog = new Dog();
+            newDog.name = "Doggie";
+            newDog.AnimalNoise();
+            Console.WriteLine(newDog.name);
+
+            Cat newCat = new Cat();
+            newCat.name = "micka";
+            newCat.AnimalNoise();
+            Console.WriteLine(newCat.name);
+            Console.ReadKey();
+        }
+    }
+}
